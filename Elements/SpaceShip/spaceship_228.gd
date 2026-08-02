5f89d3c3056515bbc79df5f1853f7a274b6d6b5c
stengvin <stengvin@mail.ru>
2026-08-02 17:06:50 +0300

    'project'

---
+++ Elements/SpaceShip/spaceship_228.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+const ROCKET_SCENE = preload("res://Elements/Misc_objects/Fireball.tscn")
+
+const SPEED = 300.0
+
+func _physics_process(delta: float):
+	if Input.is_action_just_pressed("ui_accept"):
+		shot()
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	var direction_y := Input.get_axis("ui_up", "ui_down")
+	
+	velocity.x = direction_x * SPEED
+	velocity.y = direction_y * SPEED
+	
+	move_and_slide()
+	
+func shot():
+	var rocket = ROCKET_SCENE.instantiate()
+	rocket.global_position = global_position + Vector2(0, -95)
+	add_child(rocket)
